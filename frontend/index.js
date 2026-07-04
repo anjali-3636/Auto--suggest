e75be481cf91bbd40dbdba3e01426b608025ee7b
anjali-3636 <24wh1a05al@bvrithyderabad.edu.in>
2026-07-04 10:18:27 +0530

    Random user and toggle

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,41 @@
-const express = require('express');
-const app = express();
-const port = 3002;
-app.use(express.static("frontend"))
-app.listen(port, function(){
-    console.log("sucessfully   running at http://localhost:" + port);
-})
+// Toggle Image
 function toggle() {
-    let img = document.getElementById("img");
+    const img = document.getElementById("img");
 
     if (img.style.display === "none") {
         img.style.display = "block";
     } else {
         img.style.display = "none";
     }
+}
+
+// Get Random User
+function getRandomUser() {
+    fetch("https://randomuser.me/api/")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+
+            const user = data.results[0];
+
+            // Name
+            document.getElementById("card-name").innerText =
+                user.name.first + " " + user.name.last;
+
+            // Gender
+            document.getElementById("card-gender").innerText =
+                user.gender;
+
+            // Role (Location)
+           // document.getElementById("card-role").innerText =
+            //    user.location.city + ", " + user.location.country;
+
+            // Image
+            document.getElementById("img").src =
+                user.picture.large;
+        })
+        .catch(function(error) {
+            console.log("Error:", error);
+        });
 }
