dc6bef4b5a31fd686815c811bbfc94fe98723163
anjali-3636 <24wh1a05al@bvrithyderabad.edu.in>
2026-07-04 09:28:56 +0530

    toggle

--- frontend/index.js
+++ frontend/index.js
@@ -5,3 +5,12 @@ app.use(express.static("frontend"))
 app.listen(port, function(){
     console.log("sucessfully   running at http://localhost:" + port);
 })
+function toggle() {
+    let img = document.getElementById("img");
+
+    if (img.style.display === "none") {
+        img.style.display = "block";
+    } else {
+        img.style.display = "none";
+    }
+}
